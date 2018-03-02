b6bb692d9c71268253b1602033630923b0aaed4b
pooi <ldayou@naver.com>
2018-03-02 13:51:14 +0900

    Modified gitignore

---
+++ routes/auth.js
@@ -0,0 +1,165 @@
+
+module.exports = function (app) {
+
+    var express = require('express');
+    var exec = require('child_process').exec;
+    var session = require('express-session');
+    var MySQLStore = require('express-mysql-session')(session);
+    var router = express.Router();
+    var conn = require('../config/db')();
+
+    app.use(session({
+        secret: '123IH2@OH%K2k32j4@#LBK2b5k24n',
+        resave: false,
+        saveUninitialized: true,
+        store: new MySQLStore({
+            host: 'localhost',
+            port: 3306,
+            user: 'user',
+            password: '123456',
+            database: 'o2'
+        })
+    }));
+
+    router.post('/sejong', function (req, res, next) {
+
+        const id = req.body.id;
+        const pw = req.body.password;
+
+        var sql = 'SELECT * FROM user WHERE id=?';
+        conn.query(sql, [id], function (err, results, fields) {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Internal Server Error");
+            }
+            if(results.length > 0){
+                var COMMAND = 'python3 "{0}/loginSejong.py" --id={1} --pw={2} --only=1';
+                var command = COMMAND.format(__dirname, id, pw);
+                exec(command, function(err, stdout, stderr) {
+
+                    if(err){
+                        console.log(err);
+                        res.status(500).send("Internal Server Error");
+                    }else{
+
+                        var result = stdout;
+
+                        var data = JSON.parse(result);
+                        console.log(data);
+                        if(data.status === 'fail'){
+                            res.send(data);
+                        }else {
+                            data = results[0];
+                            data.status = 'success';
+                            console.log("2", data);
+                            req.session.userData = data;
+                            console.log('session: ', req.session);
+                            req.session.save(function () {
+                                res.send(req.session.userData);
+                            });
+                        }
+
+                        // res.send(data);
+
+                    }
+
+                });
+
+            }else{
+                var COMMAND = 'python3 "{0}/loginSejong.py" --id={1} --pw={2}';
+                var command = COMMAND.format(__dirname, id, pw);
+
+                exec(command, function(err, stdout, stderr) {
+
+                    if(err){
+                        console.log(err);
+                        res.status(500).send("Internal Server Error");
+                    }else{
+
+                        var result = stdout;
+
+                        var data = JSON.parse(result);
+                        console.log(data);
+                        if(data.status === 'fail'){
+                            res.send(data);
+                        }else {
+                            delete data.status;
+
+                            var insertSql = 'INSERT INTO user SET ?';
+                            conn.query(insertSql, data, function(err, results) {
+                                if (err) {
+                                    console.log(err);
+                                    res.status(500).send("Internal Server Error");
+                                } else {
+                                    data.status = 'success';
+                                    req.session.userData = data;
+                                    console.log('session: ', req.session);
+                                    req.session.save(function () {
+                                        res.send(req.session.userData);
+                                    });
+                                }
+
+                            });
+
+
+                        }
+
+                    }
+
+                });
+            }
+        });
+
+        // var COMMAND = 'python3 "{0}/loginSejong.py" --id={1} --pw={2}';
+        // var command = COMMAND.format(__dirname, id, pw);
+        // // console.log(command);
+        //
+        // exec(command, function(err, stdout, stderr) {
+        //
+        //     // console.log(err, stdout, stderr);
+        //
+        //     if(err){
+        //         console.log(err);
+        //         res.status(500).send("Internal Server Error");
+        //     }else{
+        //
+        //         var result = stdout;
+        //
+        //         var data = JSON.parse(result);
+        //         console.log(data);
+        //         if(data.status === 'fail'){
+        //             res.send(data);
+        //         }else {
+        //             req.session.userData = data;
+        //             console.log('session: ', req.session);
+        //             req.session.save(function () {
+        //                 res.send(req.session.userData);
+        //             });
+        //         }
+        //
+        //         // res.send(data);
+        //
+        //     }
+        //
+        // });
+
+    });
+
+    router.get('/logout', function (req, res, next) {
+
+        delete req.session.userData;
+        req.session.save(function() {
+            var data = {
+                status: "success"
+            };
+            res.send(JSON.stringify(data));
+        });
+
+    });
+
+    // module.exports = router;
+    return router;
+
+};
+
+
