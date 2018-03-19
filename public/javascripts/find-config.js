00b489d595e71e5afd45e9fc8deb1c64d7e4e92c
pooi <ldayou@naver.com>
2018-03-19 09:30:10 +0900

    Add feature   * Added find page

--- public/javascripts/find-config.js
+++ public/javascripts/find-config.js
@@ -1,6 +1,6 @@
 
 
-function init() {
+function init(init_category) {
     var vue = new Vue({
         el: '#app',
         data: {
@@ -15,11 +15,64 @@ function init() {
             },
             loginData:{
             },
-            todayDate: null
-
+            todayDate: null,
+            categoryData: null,
+            searchItems: [],
+            searchSnackbar : false
         },
         methods: {
+            vueMsToDate: function (date) {
+                return msToDate(date);
+            },
+            vueMsToDateKo: function (date) {
+                return msToDateKo(date);
+            },
+            convertStatus: function (status) {
+                if(status === "WFA"){
+                    return "수거전"
+                }
+                return ""
+            },
+            hastTagsToString: function (itemData) {
+                var list = [];
+                for(var i=0; i<itemData.tags.length; i++){
+                    list.push(itemData.tags[i]);
+                }
+                for(var i=0; i<itemData.recognition_tags.length; i++){
+                    list.push(itemData.recognition_tags[i]);
+                }
 
+                var tags = "";
+                for(var i=0; i<Math.min(list.length, 5); i++){
+                    var tag = list[i];
+                    if(tag !== ""){
+                        tags += "#" + tag + " ";
+                    }
+                }
+                if(list.length > 5)
+                    tags += "...";
+
+                return tags;
+            },
+            getCategoryStringFromResult: function (title) {
+                title = title.replace(" ", "_");
+                var keys = Object.keys(this.categoryData);
+                for(var i=0; i<keys.length; i++){
+                    var key = keys[i];
+                    var subcategories = this.categoryData[key]['subcategory'];
+                    for(var j=0; j<subcategories.length; j++){
+                        var subcategory = subcategories[j];
+                        if(subcategory.name === title){
+                            return this.categoryData[key].ko + " > " + subcategory.ko;
+                            // this.subcategory = subcategory;
+                            // this.subcategories = subcategories;
+                            // this.category = this.categoryData[key];
+                            // return;
+                        }
+                    }
+                }
+                return title;
+            }
         },
         mounted: [
             function () {
@@ -36,6 +89,26 @@ function init() {
                 }
                 var today = yyyy + "-" + mm + "-" + dd; //dd + '/' + mm + '/' + yyyy;
                 this.todayDate = today;
+            },
+            function () {
+                this.categoryData = JSON.parse(init_category);
+                console.log("category: ", this.categoryData);
+            },
+            function () {
+                var data = {
+                };
+
+                axios.post(
+                    '/search',
+                    data
+                ).then(function (response) {
+                    var res = response;
+                    var data = res.data;
+                    console.log("data: ", data);
+                    vue.searchItems = vue.searchItems.concat(data);
+                }).catch(function (error) {
+                    alert(error);
+                });
             }
         ]
     });
